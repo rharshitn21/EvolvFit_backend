3f6d2903ce3e77c05620854773421693d112c0d6
rharshitn21 <rharshitn21@gmail.com>
2022-07-21 19:40:24 +0530

    First

---
+++ app.js
@@ -0,0 +1,152 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path'
+import mongoose from "mongoose";
+// import units from "./acceptedUnit.js";
+
+
+const __dirname = path.resolve();
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+const units = ["ml", "litre", "kg", "g", "item"];
+const typeOfMeal = ['Breakfast', 'Lunch', 'Evening Snack', 'Dinner'];
+
+
+mongoose.connect("mongodb://localhost:27017/mealDB");
+
+const foodItemSchema = new mongoose.Schema({
+    name: String,
+    calories: Number,
+    protien: mongoose.Decimal128,
+    carb: mongoose.Decimal128,
+    fat: mongoose.Decimal128,
+    acceptedUnits: {
+        type: String,
+        enum: units
+    },
+    itemWeight: mongoose.Decimal128
+});
+const FoodItem = mongoose.model("FoodItem", foodItemSchema);
+
+const mealSchema = new mongoose.Schema({
+    category: {
+        type:String,
+        enum:typeOfMeal,
+    },
+    name: String,
+    foodItems: [foodItemSchema]
+});
+const Meal = mongoose.model("Meal", mealSchema);
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    calorieRequirement: Number,
+    mealPlan: [{
+        date: Date,
+        mealPlan: {type: mongoose.Schema.Types.ObjectId, ref: 'Meal'}
+    }]
+});
+const User = mongoose.model("User", userSchema);
+
+
+app.get("/addfoodItem", (req, res)=>{
+    res.render("fooditem", {Itemadded: false, units: units});
+});
+
+app.get("/addmeal", (req, res) => {
+    FoodItem.find({}, (err, foodItems)=>{
+        if (err)
+            console.log(err);
+        else {
+            res.render("meal", {typeOfMeal: typeOfMeal, foodItems: foodItems});
+        }
+    });
+});
+
+app.get("/createUser", (req, res)=>{
+    Meal.find({}, (err, meals)=>{
+        if(err)
+            crossOriginIsolated.log(err);
+        else {
+            console.log(meals);
+            res.render("user", {meals: meals});
+        }
+    })
+});
+
+
+
+
+app.post("/addfooditem", (req, res)=>{
+    console.log(req.body);
+    const fooditem = new FoodItem({
+        name: req.body.name,
+        calories: req.body.calories,
+        protien: req.body.protien,
+        carb: req.body.carb,
+        fat: req.body.fat,
+        acceptedUnits: req.body.units,
+        itemWeight: req.body.itemWeight
+    });
+
+    fooditem.save((err)=>{
+        if(err)
+            res.render("fooditem", {Itemadded: false});
+        else {
+            res.render("fooditem", {Itemadded: true});
+        }
+    }); 
+});
+
+app.post("/addMeal", function (req, res){
+    if(req.body.foodItem) {
+        FoodItem.find({
+            '_id': { $in: req.body.foodItem} 
+        }, (err, embfooditem)=>{
+            if(err)
+                console.log(err);
+            else {
+                const meal = new Meal({
+                    category: req.body.typeOfMeal,
+                    name: req.body.name,
+                    foodItems: embfooditem
+                });
+
+                meal.save((err)=>{
+                    if(err) {
+                        console.log(err);
+                    }
+                    else {
+                        res.redirect("/addMeal");
+                    }
+                });
+            }
+        })
+    }
+    
+});
+
+app.post("/createUser", (req, res)=>{
+
+});
+
+app.post("/addmealplan", (req, res)=>{ 
+    
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+console.log("The server is up and running at port 3000...");
+});
